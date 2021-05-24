48116e9398e16ed59dedafa2ee276c927401354c
Ilorran <76667034+Ilorran@users.noreply.github.com>
2021-05-24 18:47:45 -0300

    Add files via upload

---
+++ ADO2_ALGPROG/src/ado2q1/ADO2Q2.java
@@ -0,0 +1,22 @@
+package ado2q2;
+
+public class ADO2Q2 {
+
+	public static void main(String[] args) {
+		int a = 1000; 
+		
+		
+		for(a = 1000; a <= 9999; a+=1)
+			
+		if(a == (((a / 100) + (a % 100)) * ((a / 100) + (a % 100)))) {
+		
+		 
+		 
+			 System.out.print(a); 
+		 
+		 	a++;
+
+	}
+
+}
+}
